dec81248772bae8328e260131752db710910144a
Brock Denton <brock@brockdenton.com>
2025-09-19 13:48:44 -0400

    Initial Commit

---
+++ PhotoBridge/PhotoBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoBridgeApp.swift
+//  PhotoBridge
+//
+//  Created by Brock Denton on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoBridgeTests/PhotoBridgeTests.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoBridgeTests.swift
+//  PhotoBridgeTests
+//
+//  Created by Brock Denton on 9/19/25.
+//
+
+import Testing
+
+struct PhotoBridgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
